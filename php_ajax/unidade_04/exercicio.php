4d4ddaaed0e015b5408165971e96c0cf7be1dd65
unknown <wilhamsmeira@gmail.com>
2020-09-12 17:08:31 -0300

    list whit ajax and jquery

--- php_ajax/unidade_04/exercicio.php
+++ php_ajax/unidade_04/exercicio.php
@@ -1,16 +1,44 @@
 <!doctype html>
 <html>
-    <head>
-        <meta charset="UTF-8">
-        <title>PHP com AJAX</title>
-    </head>
 
-    <body>
-        <div id="listagem"></div>
-        
-        <script src="jquery.js"></script>
-        <script>
+<head>
+    <meta charset="UTF-8">
+    <title>PHP com AJAX</title>
+</head>
 
-        </script>
-    </body>
+<body>
+    <div id="listagem"></div>
+    <div id="gg"></div>
+
+    <script src="jquery.js"></script>
+    <script>
+        $.ajax({
+            url: '_xml/produtos.xml'
+        }).then(sucesso, falha)
+
+        function sucesso(x) {
+            console.log($(x).find('produto'));
+            console.log("Aqui");
+            console.log($(x).find('precounitario1'));
+            $('#gg').html("ok");
+            var elemento;
+            elemento = "<ul>";
+            $(x).find('produto').each(function() {
+                var nome = $(this).find('nomeproduto').text();
+                var preco = $(this).find('precounitario').text();
+                elemento += "<li>" + nome + " - R$  " + preco + "</li>";
+            });
+
+            elemento += "<ul>";
+            $('#listagem').html(elemento);
+
+
+        }
+
+        function falha() {
+
+        }
+    </script>
+</body>
+
 </html>
