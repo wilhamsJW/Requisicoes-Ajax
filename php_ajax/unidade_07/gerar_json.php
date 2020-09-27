0e56d25c58cee049ee1a6063cf471e2c16da5801
unknown <wilhamsmeira@gmail.com>
2020-09-27 17:10:06 -0300

    json_encode/ transforma array em json

--- php_ajax/unidade_07/gerar_json.php
+++ php_ajax/unidade_07/gerar_json.php
@@ -8,13 +8,17 @@
     $selecao = "SELECT nomeproduto, precounitario, imagempequena FROM produtos";
     $produtos = mysqli_query($conecta,$selecao);
 
-    $retorno = array();
+    $retorno;
     while($linha = mysqli_fetch_object($produtos)) {
         $retorno[] = $linha;
     }   
 
     echo json_encode($retorno);
+    echo json_encode($retorno[0]); //caso queira buscar algum item em si dentro do array, seria com [0]
     
+    /**Json_encode transforma um array vindo do db em formato JSON*/
+    
+
     // fechar conecta
     mysqli_close($conecta);
 ?>
